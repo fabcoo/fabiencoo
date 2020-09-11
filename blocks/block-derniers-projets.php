51b8e193ffff5031c973093bdd53c2e7643e362b
Fabien Coo <fabien.coo@amigraf.com>
2020-09-11 09:05:21 +0200

    Réindentation du fichier de blocs personnalisés

--- blocks/block-derniers-projets.php
+++ blocks/block-derniers-projets.php
@@ -4,7 +4,7 @@
 $post_count = block_value( 'number' );
 $args = array(
 	'post_type'      => array( 'projet' ),
-  'posts_per_page' => $post_count,
+	'posts_per_page' => $post_count,
 );
 
 // The Query
@@ -17,10 +17,10 @@ if ( $query->have_posts() ) {?>
 		<article class="col-md-4" data-aos="fade-up">
 			<div class="projet" style="background-image: linear-gradient(to bottom, rgba(0,0,0,0.25) 0%,rgba(0,0,0,0.85) 100%),url('<?php the_post_thumbnail_url() ?>')">
 				<a href="<?php the_permalink() ?>">
-	        <h3><?php the_title() ?></h3>
-	      </a>
+					<h3><?php the_title() ?></h3>
+				</a>
 			</div>
-    </article>
+    	</article>
 	<?php }
 } else {
 	// no posts found
@@ -29,11 +29,11 @@ if ( $query->have_posts() ) {?>
 <?php // Restore original Post Data
 wp_reset_postdata();
  ?>
- <article class="col-md-4 voirPortfolio" data-aos="fade-up">
-	 <div class="projet">
-		 <a href="<?php echo home_url() ?>/portfolio">
-       <h3>Voir mes autres projets <i class="fas fa-arrow-right"></i></h3>
-     </a>
-	 </div>
- </article>
+	<article class="col-md-4 voirPortfolio" data-aos="fade-up">
+		<div class="projet">
+			<a href="<?php echo home_url() ?>/portfolio">
+				<h3>Voir mes autres projets <i class="fas fa-arrow-right"></i></h3>
+			</a>
+		</div>
+	</article>
 </section>
