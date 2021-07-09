bb9417ee43334036dac668d38736091d8f89d131
Fabien Coo <fabien.coo@amigraf.com>
2021-07-09 10:20:42 +0200

    Ajout de smooth scrolling

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,67 +1,78 @@
 //Début code jQuery
-jQuery(document).ready(function($) {
-  // Initialisation Animate On Scroll
-  AOS.init();
+jQuery(document).ready(function ($) {
+	// Initialisation Animate On Scroll
+	AOS.init();
 
-  // Ajout d'attributs AOS sur certaines classes
-  $('.fade-up').attr('data-aos', 'fade-up');
-  $('.fade-right').attr('data-aos', 'fade-right');
-  $('.fade-left').attr('data-aos', 'fade-left');
+	// Ajout d'attributs AOS sur certaines classes
+	$(".fade-up").attr("data-aos", "fade-up");
+	$(".fade-right").attr("data-aos", "fade-right");
+	$(".fade-left").attr("data-aos", "fade-left");
 
+	// Apparition/disparition des menus
+	$(".toggle-formation").click(function () {
+		if ($("#site-navigation").hasClass("site-open")) {
+			$("#site-navigation").removeClass("site-open");
+			$("#site-navigation").addClass("site-close");
+		}
+		$("#formation-navigation").addClass("formation-open");
+		$("#formation-navigation").removeClass("formation-close");
+	});
+	$(".toggle-site").click(function () {
+		if ($("#formation-navigation").hasClass("formation-open")) {
+			$("#formation-navigation").removeClass("formation-open");
+			$("#formation-navigation").addClass("formation-close");
+		}
+		$("#site-navigation").addClass("site-open");
+		$("#site-navigation").removeClass("site-close");
+	});
 
-  // Apparition/disparition des menus
-  $('.toggle-formation').click(function() {
-    if ($('#site-navigation').hasClass('site-open')) {
-      $('#site-navigation').removeClass('site-open');
-      $('#site-navigation').addClass('site-close');
-    }
-    $('#formation-navigation').addClass('formation-open');
-    $('#formation-navigation').removeClass('formation-close');
-  });
-  $('.toggle-site').click(function() {
-    if ($('#formation-navigation').hasClass('formation-open')) {
-      $('#formation-navigation').removeClass('formation-open');
-      $('#formation-navigation').addClass('formation-close');
-    }
-    $('#site-navigation').addClass('site-open');
-    $('#site-navigation').removeClass('site-close');
-  });
+	$(".fa-times").click(function () {
+		if ($("#site-navigation").hasClass("site-open")) {
+			$("#site-navigation").removeClass("site-open");
+			$("#site-navigation").addClass("site-close");
+		}
+	});
+	$(".fa-times").click(function () {
+		if ($("#formation-navigation").hasClass("formation-open")) {
+			$("#formation-navigation").removeClass("formation-open");
+			$("#formation-navigation").addClass("formation-close");
+		}
+	});
+	$("#menu-item-87").click(function () {
+		if ($("#site-navigation").hasClass("site-open")) {
+			$("#site-navigation").removeClass("site-open");
+			$("#site-navigation").addClass("site-close");
+		}
+	});
 
-  $('.fa-times').click(function() {
-    if ($('#site-navigation').hasClass('site-open')) {
-      $('#site-navigation').removeClass('site-open');
-      $('#site-navigation').addClass('site-close');
-    }
-  });
-  $('.fa-times').click(function() {
-    if ($('#formation-navigation').hasClass('formation-open')) {
-      $('#formation-navigation').removeClass('formation-open');
-      $('#formation-navigation').addClass('formation-close');
-    }
-  });
-  $('#menu-item-87').click(function() {
-    if ($('#site-navigation').hasClass('site-open')) {
-      $('#site-navigation').removeClass('site-open');
-      $('#site-navigation').addClass('site-close');
-    }
-  });
+	// Ajout d'une couleur de fond sur header au Scroll
+	$(function () {
+		var header = $(".site-header");
+		$(window).scroll(function () {
+			var scroll = $(window).scrollTop();
 
+			if (scroll >= 50) {
+				header.removeClass("header").addClass("bg");
+			} else {
+				header.removeClass("bg").addClass("header");
+			}
+		});
+	});
 
-  // Ajout d'une couleur de fond sur header au Scroll
-  $(function() {
-    var header = $(".site-header");
-    $(window).scroll(function() {
-      var scroll = $(window).scrollTop();
-
-      if (scroll >= 50) {
-        header.removeClass('header').addClass("bg");
-      } else {
-        header.removeClass("bg").addClass('header');
-      }
-    });
-  });
+	// Ajout d'un smooth scrolling avec arrêt du scroll à la barre de header
+	$("a[href*='#']").on("click", function () {
+		var page = $(this).attr("href");
+		var speed = 250;
+		$("html, body").animate(
+			{
+				scrollTop: $(page).offset().top - 111,
+			},
+			speed
+		);
+		return false;
+	});
 });
 
-jQuery(window).load(function($) {
-  AOS.refreshHard();
+jQuery(window).load(function ($) {
+	AOS.refreshHard();
 });
