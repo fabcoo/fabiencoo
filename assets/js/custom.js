b0871b0b1c0adc0c1d3bd0dadc4c25369f97926d
Fabien Coo <fabien.coo@amigraf.com>
2021-07-09 10:44:18 +0200

    Test CSS décalage lien ancrés

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -60,25 +60,6 @@ jQuery(document).ready(function ($) {
 	});
 });
 
-// The function actually applying the offset
-function offsetAnchor() {
-	if (location.hash.length !== 0) {
-		window.scrollTo(window.scrollX, window.scrollY - 100);
-	}
-}
-
-// Captures click events of all a elements with href starting with #
-jQuery(document).on("click", 'a[href^="#"]', function (event) {
-	// Click events are captured before hashchanges. Timeout
-	// causes offsetAnchor to be called after the page jump.
-	window.setTimeout(function () {
-		offsetAnchor();
-	}, 0);
-});
-
-// Set the offset when entering page with hash present in the url
-window.setTimeout(offsetAnchor, 0);
-
 jQuery(window).load(function () {
 	AOS.refreshHard();
 });
