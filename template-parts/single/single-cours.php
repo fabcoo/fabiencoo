77b54191fb2cdb5607baf3a7172c637b03f3f23f
Fabien Coo <fabien.coo@amigraf.com>
2021-11-30 10:17:35 +0100

    Ajout de rôles utilisateurs aux cours

--- template-parts/single/single-cours.php
+++ template-parts/single/single-cours.php
@@ -12,24 +12,39 @@
 <?php $user = wp_get_current_user();
 if ( $user->roles[0] == 'um_formation-continue' && has_term('formation-continue','restriction') ): ?>
 <header class="container page-header">
-		<h1>Contenu restreint</h1>
-		<p>Le formateur n'a pas encore autorisé l'accès à ce cours pour le moment.</p>
+    <h1>Contenu restreint</h1>
+    <p>Le formateur n'a pas encore autorisé l'accès à ce cours pour le moment.</p>
 </header>
 <?php elseif ( $user->roles[0] == 'apprentissage' && has_term('apprentissage','restriction') ):?>
-	<header class="container page-header">
-			<h1>Contenu restreint</h1>
-			<p>Le formateur n'a pas encore autorisé l'accès à ce cours pour le moment.</p>
-	</header>
+<header class="container page-header">
+    <h1>Contenu restreint</h1>
+    <p>Le formateur n'a pas encore autorisé l'accès à ce cours pour le moment.</p>
+</header>
+<?php elseif ( $user->roles[0] == 'ms_cm_1' && has_term('ms-cm-1','restriction') ):?>
+<header class="container page-header">
+    <h1>Contenu restreint</h1>
+    <p>Le formateur n'a pas encore autorisé l'accès à ce cours pour le moment.</p>
+</header>
+<?php elseif ( $user->roles[0] == 'ms_cm_2' && has_term('ms-cm-2','restriction') ):?>
+<header class="container page-header">
+    <h1>Contenu restreint</h1>
+    <p>Le formateur n'a pas encore autorisé l'accès à ce cours pour le moment.</p>
+</header>
+<?php elseif ( $user->roles[0] == 'imep' && has_term('imep','restriction') ):?>
+<header class="container page-header">
+    <h1>Contenu restreint</h1>
+    <p>Le formateur n'a pas encore autorisé l'accès à ce cours pour le moment.</p>
+</header>
 <?php else: ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class('container'); ?>>
-	<div class="row">
+    <div class="row">
 
-		<div class="col-sm-8">
-			<p class="numero">Chapitre <?php the_field('numero_exercice') ?></p>
-			<h1><?php the_title() ?></h1>
-		</div>
-		<div class="col-sm-4">
-			<?php
+        <div class="col-sm-8">
+            <p class="numero">Chapitre <?php the_field('numero_exercice') ?></p>
+            <h1><?php the_title() ?></h1>
+        </div>
+        <div class="col-sm-4">
+            <?php
 				$matiere = wp_get_object_terms( $post->ID,  'matiere' );
 				if ( ! empty( $matiere ) ) {
 					if ( ! is_wp_error( $matiere ) ) {
@@ -38,40 +53,56 @@ if ( $user->roles[0] == 'um_formation-continue' && has_term('formation-continue'
 						 }
 					}
 				} ?>
-		</div>
-		<div class="col-sm-8">
-			<h2>Notions</h2>
-			<?php the_field('notions') ?>
-			<h2>Instructions</h2>
-			<?php the_field('instructions') ?>
-		</div>
-		<div class="col-sm-4 galerie">
-			<?php
+        </div>
+        <div class="col-sm-8">
+            <h2>Notions</h2>
+            <?php the_field('notions') ?>
+            <h2>Instructions</h2>
+            <?php the_field('instructions') ?>
+        </div>
+        <div class="col-sm-4 galerie">
+            <?php
 				$images = get_field('galerie');
 				if( $images ): ?>
-        <?php foreach( $images as $image ): ?>
-          <a data-fancybox="galerie" href="<?php echo esc_url($image['url']); ?>">
-               <img src="<?php echo esc_url($image['sizes']['large']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
-          </a>
-      	<?php endforeach; ?>
-			<?php endif; ?>
-			<?php if(get_field('sources')): ?>
-				<a href="<?php the_field('sources') ?>" class="sources">Télécharger les sources</a>
-			<?php endif; ?>
-			<!-- Affichage conditionnel de la correction -->
-			<?php if(get_field('mot_de_passe_correction')): ?>
-				<!-- Affichage du mot de passe si la case est coché et que le rôle est "formation continue" -->
-				<?php if($user->roles[0] == 'um_formation-continue' && get_field('correction_formation_continue') == true ): ?>
-					<a href="<?php the_field('correction') ?>" class="correction">Télécharger la correction</a>
-					<p class="mdp">Mot de passe :<br /><strong><?php the_field('mot_de_passe_correction') ?></strong></p>
-				<?php endif; ?> 
-				<!-- Affichage du mot de passe si la case est coché et que le rôle est "apprentissage" -->
-				<?php if($user->roles[0] == 'apprentissage' && get_field('correction_apprentissage') == true ): ?>
-					<a href="<?php the_field('correction') ?>" class="correction">Télécharger la correction</a>
-					<p class="mdp">Mot de passe :<br /><strong><?php the_field('mot_de_passe_correction') ?></strong></p>
-				<?php endif; ?>
-			<?php endif; ?>
-		</div>
-	</div>
+            <?php foreach( $images as $image ): ?>
+            <a data-fancybox="galerie" href="<?php echo esc_url($image['url']); ?>">
+                <img src="<?php echo esc_url($image['sizes']['large']); ?>"
+                    alt="<?php echo esc_attr($image['alt']); ?>" />
+            </a>
+            <?php endforeach; ?>
+            <?php endif; ?>
+            <?php if(get_field('sources')): ?>
+            <a href="<?php the_field('sources') ?>" class="sources">Télécharger les sources</a>
+            <?php endif; ?>
+            <!-- Affichage conditionnel de la correction -->
+            <?php if(get_field('mot_de_passe_correction')): ?>
+            <!-- Affichage du mot de passe si la case est coché et que le rôle est "formation continue" -->
+            <?php if($user->roles[0] == 'um_formation-continue' && get_field('correction_formation_continue') == true ): ?>
+            <a href="<?php the_field('correction') ?>" class="correction">Télécharger la correction</a>
+            <p class="mdp">Mot de passe :<br /><strong><?php the_field('mot_de_passe_correction') ?></strong></p>
+            <?php endif; ?>
+            <!-- Affichage du mot de passe si la case est coché et que le rôle est "apprentissage" -->
+            <?php if($user->roles[0] == 'apprentissage' && get_field('correction_apprentissage') == true ): ?>
+            <a href="<?php the_field('correction') ?>" class="correction">Télécharger la correction</a>
+            <p class="mdp">Mot de passe :<br /><strong><?php the_field('mot_de_passe_correction') ?></strong></p>
+            <?php endif; ?>
+            <!-- Affichage du mot de passe si la case est coché et que le rôle est "MS CM 1" -->
+            <?php if($user->roles[0] == 'ms_cm_1' && get_field('correction_ms_cm_1') == true ): ?>
+            <a href="<?php the_field('correction') ?>" class="correction">Télécharger la correction</a>
+            <p class="mdp">Mot de passe :<br /><strong><?php the_field('mot_de_passe_correction') ?></strong></p>
+            <?php endif; ?>
+            <!-- Affichage du mot de passe si la case est coché et que le rôle est "MS CM 2" -->
+            <?php if($user->roles[0] == 'ms_cm_2' && get_field('correction_ms_cm_2') == true ): ?>
+            <a href="<?php the_field('correction') ?>" class="correction">Télécharger la correction</a>
+            <p class="mdp">Mot de passe :<br /><strong><?php the_field('mot_de_passe_correction') ?></strong></p>
+            <?php endif; ?>
+            <!-- Affichage du mot de passe si la case est coché et que le rôle est "MS CM 1" -->
+            <?php if($user->roles[0] == 'imep' && get_field('correction_imep') == true ): ?>
+            <a href="<?php the_field('correction') ?>" class="correction">Télécharger la correction</a>
+            <p class="mdp">Mot de passe :<br /><strong><?php the_field('mot_de_passe_correction') ?></strong></p>
+            <?php endif; ?>
+            <?php endif; ?>
+        </div>
+    </div>
 </article><!-- #post-<?php the_ID(); ?> -->
 <?php endif; ?>
